27bddb6ca1690711401b144fe3ab828ec692ab10
Sergej Frank <sfrank@Sergejs-MacBook-Pro.local>
2018-10-12 23:38:55 +0200

    street and car entrance

---
+++ Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour {
+
+    private BoxCollider2D streetCollider;
+    public float verticalLength;
+
+    // Use this for initialization
+    void Awake () {
+        streetCollider = GetComponent<BoxCollider2D>();
+        verticalLength = streetCollider.size.y;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (transform.position.y < - verticalLength){
+            Reposition();
+        }
+    }
+
+    void Reposition(){
+        Vector2 groundOffset = new Vector2(0, verticalLength * 2f);
+        transform.position = (Vector2)transform.position + groundOffset;
+    }
+}
---
+++ Assets/Scripts/DriveIn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveIn : MonoBehaviour {
+    private float yVelocity = 0.0f;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+    void Update () {
+
+        float newPosition = Mathf.SmoothDamp(transform.position.y, 0, ref yVelocity, 0.9f);
+        transform.position = new Vector3(0, newPosition,0);
+
+    }
+}
