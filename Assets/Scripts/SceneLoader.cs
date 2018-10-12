eac6068a155bad40ca385942ccca3e7861686d41
lubernha <luca@luca.wtf>
2018-10-12 23:47:56 +0200

    fuegt SceneController und SceneLoader hinzu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(int level)
+    {
+        Debug.Log("Load Level: " + level);
+    }
+
+    public void LoadScene(string levelName) {
+        Debug.Log("Load Level(name): " + levelName);
+    }
+
+}
