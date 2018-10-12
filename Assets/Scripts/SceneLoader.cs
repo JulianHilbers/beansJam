8ee80c5d74d9075650a5f127e49731bc454387fc
lubernha <luca@luca.wtf>
2018-10-13 00:24:06 +0200

    wechselst scenen

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
     public void LoadScene(int level)
     {
         Debug.Log("Load Level: " + level);
+        SceneManager.LoadScene(level);
+
     }
 
     public void LoadScene(string levelName) {
         Debug.Log("Load Level(name): " + levelName);
+        SceneManager.LoadScene(levelName);
     }
 
 }
