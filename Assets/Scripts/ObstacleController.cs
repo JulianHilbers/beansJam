b8cdd7cf02925164a45e6475dd6be658909834e7
Jan Klecka <jan.klecka@otto.de>
2018-10-13 12:25:24 +0200

    added second obstacle and make lanes random

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour
 {
-
+    public int position = 2;
     public int speed;
     public List<GameObject> list;
 
@@ -14,29 +14,51 @@ public class ObstacleController : MonoBehaviour
 
     private void Start()
     {
+        float lastYpos = -10;
+        int lastLane = 0;
         list.ForEach((GameObject obj) => {
             GameObject clone = Object.Instantiate(obj);
-            int randomPos = Random.Range(0, lanes.Length);
-            clone.transform.position = new Vector3(lanes[randomPos], -10, 0);
+
+            int newLane = GetLane(lastLane);
+            clone.transform.position = new Vector3(lanes[newLane], lastYpos, 0);
 
             cloneList.Add(new Obstacle() {
                 transform = clone.transform,
                 active = true,
                 renderer = clone.GetComponent<Renderer>()
             });
-
+            lastYpos -= position;
+            Debug.Log(lastYpos);
+            lastLane = newLane;
         });
+    }
+
+    private int GetLane(int lastLane)
+    {
+        bool searchingNewLane = true;
+        int newLane = 0;
+        while (searchingNewLane)
+        {
+            newLane = Random.Range(0, lanes.Length);
+            if (newLane != lastLane)
+            {
+                searchingNewLane = false;
+            }
+        }
+        return newLane;
     }
 
     void FixedUpdate()
     {
+        int lastLane = 0;
         cloneList.ForEach((Obstacle obstacle) => {
             if (obstacle.active) {
                 obstacle.transform.Translate(Vector2.down * Time.deltaTime * speed);
                 
                 if (!obstacle.renderer.isVisible && obstacle.renderer.transform.position.y < -2f) {
-                    int randomPos = Random.Range(0, lanes.Length);
-                    obstacle.transform.position = new Vector3(lanes[randomPos], 10, 0);
+                    int newLane = GetLane(lastLane);
+                    obstacle.transform.position = new Vector3(lanes[newLane], 10, 0);
+                    lastLane = newLane;
                 }
             }
 
