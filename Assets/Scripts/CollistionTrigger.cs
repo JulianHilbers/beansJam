8affa6b10d4f82dd3126b52d2dec3c26d39bd1ec
Sergej Frank <sfrank@Sergejs-MacBook-Pro.local>
2018-10-13 12:29:15 +0200

    add forgotten scene

---
+++ Assets/Scripts/CollistionTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollistionTrigger : MonoBehaviour {
+
+    private BoxCollider2D bc2d;
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start () {
+        bc2d = GetComponent<BoxCollider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Something has entered this zone.");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
