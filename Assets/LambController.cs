c43d8a839758dcf4da3543a7c27156691e5bf35e
hilbers.julian@gmail.com <hilbers.julian@gmail.com>
2018-10-13 17:20:18 +0200

    build scene extended.

--- Assets/LambController.cs
+++ Assets/LambController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class LambController : MonoBehaviour
 {
     public float moveSteps = 1f;
+    public float failPosition = -3.5f;
+
+    [Header("Scene")]
+    public string sceneName;
+    public SceneLoader SceneLoader;
 
     private Rigidbody2D rBody;
     private float initialY;
@@ -18,11 +23,21 @@ public class LambController : MonoBehaviour
     public void MoveUp()
     {
         rBody.AddForce(Vector2.up * moveSteps);
+
+        CheckForFail();
     }
 
     public void MoveDown()
     {
         if (transform.position.y >= initialY)
             rBody.AddForce(Vector2.down * moveSteps);
+
+        CheckForFail();
+    }
+
+    private void CheckForFail()
+    {
+        if (transform.position.y > failPosition)
+            SceneLoader.LoadScene(sceneName);
     }
 }
