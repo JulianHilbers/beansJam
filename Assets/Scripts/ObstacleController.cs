4dbd85e3093af2ada574df87ed043614f1ff931d
hilbers.julian@gmail.com <hilbers.julian@gmail.com>
2018-10-13 15:18:37 +0200

    waiting for obstacles implemented.

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -5,28 +5,21 @@ using UnityEngine;
 public class ObstacleController : MonoBehaviour
 {
     public float position = 20f;
+    public float waitUntilFirtstSpawn = 4f;
+
     public int speed;
     public List<GameObject> list;
     public List<GameObject> clonedList;
 
-    private float[] lanes = new float[]{-2.5f, 0.0f, 2.5f};
 
+    private float waitedTime = 0;
+    private float[] lanes = new float[] { -2.5f, 0.0f, 2.5f };
+
+    private bool initialized = false;
 
     private void Start()
     {
-        float lastYpos = 10;
-        int lastLane = 0;
-        list.ForEach((GameObject obj) => {
-            GameObject clone = Object.Instantiate(obj);
 
-            int newLane = GetLane(lastLane);
-            clone.transform.position = new Vector3(lanes[newLane], lastYpos, 0);
-
-            clonedList.Add(clone);
-
-            lastYpos -= position;
-            lastLane = newLane;
-        });
     }
 
     private int GetLane(int lastLane)
@@ -46,13 +39,51 @@ public class ObstacleController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (waitedTime < waitUntilFirtstSpawn)
+            waitedTime += Time.deltaTime;
+
+
+        if (waitedTime >= waitUntilFirtstSpawn)
+        {
+            if (!initialized)
+            {
+                initialized = true;
+                InitializeObstacles();
+            }
+
+            MoveObstacles();
+        }
+    }
+
+    private void MoveObstacles()
+    {
         int lastLane = 0;
-        clonedList.ForEach((GameObject obj) => {
-            if (!obj.GetComponent<Spawnable>().isActive()) {
+        clonedList.ForEach((GameObject obj) =>
+        {
+            if (!obj.GetComponent<Spawnable>().isActive())
+            {
                 int newLane = GetLane(lastLane);
                 obj.GetComponent<Spawnable>().ReSpawn(newLane);
                 lastLane = newLane;
             }
+        });
+    }
+
+    private void InitializeObstacles()
+    {
+        float lastYpos = 14;
+        int lastLane = 0;
+        list.ForEach((GameObject obj) =>
+        {
+            GameObject clone = Instantiate(obj);
+
+            int newLane = GetLane(lastLane);
+            clone.transform.position = new Vector3(lanes[newLane], lastYpos, 0);
+
+            clonedList.Add(clone);
+
+            lastYpos -= position;
+            lastLane = newLane;
         });
     }
 }
