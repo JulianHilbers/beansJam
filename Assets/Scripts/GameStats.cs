522a69c0031fe61bbdfcec8805e9f81ebf108730
lubernha <luca@luca.wtf>
2018-10-13 13:09:55 +0200

    GameStats scriptable object

---
+++ Assets/Scripts/GameStats.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameStats : ScriptableObject {
+    private int CurrentScore;
+
+    private List<int> highscores;
+
+    public void OnEnable() {
+        highscores = new List<int>();
+    }
+
+    public void SetCurrentScore(int newScore) {
+        if (CurrentScore > 0) {
+            highscores.Add(CurrentScore);
+        }
+        CurrentScore = newScore;
+    }
+
+    public int GetCurrentScore() {
+        return CurrentScore;
+    }
+
+    public List<int> GetHighscores() {
+        return highscores;
+    }
+}
