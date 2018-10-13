16290b436d36d4e4c39667a331de2f319d4f8165
Sergej Frank <sfrank@Sergejs-MacBook-Pro.local>
2018-10-13 18:30:28 +0200

    shake

--- Assets/Scripts/CollistionTrigger.cs
+++ Assets/Scripts/CollistionTrigger.cs
@@ -2,26 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CollistionTrigger : MonoBehaviour {
 
     private BoxCollider2D bc2d;
     private Rigidbody2D rb2d;
     private Animator animator;
 
+
+
     // Use this for initialization
     void Start () {
+
+
         bc2d = GetComponent<BoxCollider2D>();
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-	}
+    }
 
     void OnTriggerEnter2D(Collider2D other){
         Spawnable spawnable = other.GetComponent<Spawnable>();
         if (other.tag.Equals("PowerUp")){
             spawnable.GetComponent<PowerUp>().OnHit();
         }
-        if (other.tag.Equals("Obstacle"))
-        {
+        if (other.tag.Equals("Obstacle")){
+            Camera.main.GetComponent<CameraControl>().Shake(0.2f, 4, 100*Time.deltaTime);
+
             rb2d.AddForce(new Vector3(0, -100));
             animator.SetTrigger("CrashTrigger");
             string colliderTag = other.tag;
@@ -30,8 +36,9 @@ public class CollistionTrigger : MonoBehaviour {
        
     }
 
+
     // Update is called once per frame
     void Update () {
-		
-	}
+      
+    }
 }
