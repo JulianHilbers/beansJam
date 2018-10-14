a019baf02d52e187c72a9a9aa5460cfb31862944
lubernha <luca@luca.wtf>
2018-10-14 19:01:43 +0200

    sounds

---
+++ Assets/Scripts/Intro/SoundChapter1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundChapter1 : MonoBehaviour {
+
+    private float chapterStartTime;
+
+    public AudioSource door;
+    public float doorTime;
+
+    private void OnEnable() {
+        chapterStartTime = Time.time;
+
+        door.Play((ulong) doorTime);
+        //door.PlayDelayed(doorTime);
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+}
