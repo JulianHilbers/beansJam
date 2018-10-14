c8b68d1b4f4f357641680509d3ad8d511829a401
Jan Klecka <jan.klecka@otto.de>
2018-10-14 13:53:05 +0200

    add obstale1 break animation

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,5 +3,18 @@ using System.Collections;
 
 public class Obstacle : Spawnable
 {
+    public void OnHit()
+    {
+        if (GetComponent<Animator>()) {
+            GetComponent<Animator>().SetTrigger("obstacleHit");
+        }
+    }
 
+    public override void ReSpawn(int lane)
+    {
+        if (GetComponent<Animator>()) {
+            GetComponent<Animator>().SetTrigger("resetHit");
+        }
+        base.ReSpawn(lane);
+    }
 }
