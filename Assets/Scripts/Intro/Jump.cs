8dbb3ba92e13bbf7fcaed976df45772c18369f84
lubernha <luca@luca.wtf>
2018-10-14 18:09:10 +0200

    story scene chapter 2

---
+++ Assets/Scripts/Intro/Jump.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+    public float timeUntilJump;
+    public float jumpHeight;
+    private Vector3 jumpTargetPosition;
+    public Vector3 startPosition;
+    private float chapterStartTime;
+    private State state = State.STAND;
+
+
+    private void OnEnable() {
+        chapterStartTime = Time.time;
+        //startPosition = transform.position;
+
+        jumpTargetPosition = new Vector3(transform.position.x, transform.position.y + jumpHeight, transform.position.z);
+    }
+
+    private void FixedUpdate() {
+        if(state != State.DOWN && state != State.NUFFIN && chapterStartTime + timeUntilJump < Time.time) {
+            state = State.UP;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (state == State.UP && IsAtTarget())
+        {
+            state = State.DOWN;
+        }
+        if (state == State.UP) {
+            transform.position = Vector3.MoveTowards(transform.position, jumpTargetPosition, 12f * Time.deltaTime);
+        }
+        if(state == State.DOWN) {
+            transform.position = Vector3.MoveTowards(transform.position, startPosition, 12f * Time.deltaTime);
+            if (IsAtTarget()) {
+                state = State.NUFFIN;
+            }
+        }
+	}
+
+    private bool IsAtTarget() {
+        Vector3 t = state == State.UP ? jumpTargetPosition : startPosition;
+        return Vector3.Distance(transform.position, t) < .05f;
+    }
+
+    enum State {
+        STAND,
+        UP,
+        DOWN,
+        NUFFIN
+    }
+}
---
+++ Assets/Scripts/Intro/Stiften.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stiften : MonoBehaviour {
+
+    public float timeUntilJump;
+    public float runLength;
+    private Vector3 runTargetPosition;
+    private float chapterStartTime;
+    private State state = State.STAND;
+
+
+    private void OnEnable()
+    {
+        chapterStartTime = Time.time;
+
+        runTargetPosition = new Vector3(transform.position.x + runLength, transform.position.y, transform.position.z);
+    }
+
+    private void FixedUpdate()
+    {
+        if (state == State.STAND && chapterStartTime + timeUntilJump < Time.time)
+        {
+            state = State.RUN;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.RUN)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, runTargetPosition, 5f * Time.deltaTime);
+        }
+    }
+
+
+    enum State
+    {
+        STAND,
+        RUN
+    }
+}
